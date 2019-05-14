5b480643a8867ab5d2f71b83218e12811abf295c
Florian <florian.schmidt.welzow@t-online.de>
2019-05-14 19:53:26 +0200

    phpunit and php binaries and some refactoring

---
+++ lib/php.lib.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+PHP_CONTAINER="winpty docker exec -it -w //code//w mediawiki_php-fpm_1 sh -c "
+
+#
+# Executes a PHP script.
+#
+# Usages:
+#  php PHP_ARGUMENTS
+#  php WORKING_DIRECTORY PHP_ARGUMENTS
+#
+function php {
+	if [[ -z "$2" ]]; then
+		php_args=$1
+	else
+		cwd=$1
+		php_args=$2
+		command_string="cd $cwd &&"
+	fi
+	command_string="$command_string php $php_args"
+
+	$PHP_CONTAINER "$command_string"
+}
+
+#
+# Executes composer.
+#
+# Usages:
+#  composer COMPOSER_ARGUMENTS
+#
+function composer {
+	$PHP_CONTAINER "composer $@"
+}
